a5df715cb58ef83785bddf7e260ba0f018715c92
avinash <avinashyadav145053@gmail.com>
2022-06-28 18:28:18 +0530

    updated settings

--- project1/settings.py
+++ project1/settings.py
@@ -94,13 +94,24 @@ WSGI_APPLICATION = 'project1.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+if DEBUG :
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME':'ciba',
+        DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME':BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+
+
+else:
+
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME':'ciba',
+        }
+    }
 
 CACHES = {
     'default': {
